c25b71052149444737fbca5f09e322581cb7f908
Christoph A. Roedig <chris@roedig.us>
2021-01-18 22:33:12 -0500

    Create main.py

---
+++ VAOccupancy/main.py
@@ -0,0 +1,27 @@
+import requests
+import re as reg
+
+IFRAME_URL = 'https://portal.rockgympro.com/portal/public/bc4d3be86f2f8564a4e5e4f9151f6bf6/occupancy?iframeid=occupancyCounter&fId=1068'
+
+def handler(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+    r = requests.get(IFRAME_URL, verify=False)
+    raw_data = r.text.encode('utf-8').decode('unicode_escape')
+    data = {
+        'count': reg.search("\\\'count\\\' : (.*),", raw_data)[1],
+        'capacity': reg.search("\\\'capacity\\\' : (.*),", raw_data)[1]
+    }
+    greeting = 'Head over and Climb On!'
+    if(float(data['count'])/float(data['capacity']) >= 0.75 ):
+        greeting = 'Better hurry up, lots of people here.'
+    if(float(data['count'])/float(data['capacity']) >= 0.95 ):
+        greeting = '🤬 The gym is full.'
+    
+    return f'{greeting} There are {data["count"]} people climing right now.'
